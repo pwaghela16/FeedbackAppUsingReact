f56ffedfeaae28b93f9e7f5df5b6755a4f19662b
pwaghela16 <110381561+pwaghela16@users.noreply.github.com>
2022-10-05 12:10:56 +0530

    Add files via upload
    added files

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,65 @@
+import { createContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: "This is feedback item 1",
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: "This is feedback item 2",
+      rating: 5,
+    },
+    {
+      id: 3,
+      text: "This is feedback item 3",
+      rating: 7,
+    },
+  ]);
+  const [feedbackEdit, setfeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  // Delete Feedback
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  // Add Feedback
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+  // Edit feedback
+  const editFeedback = (item) => {
+    setfeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
+  // update feedback
+
+  const updateFeedback =(id, updItem) => {
+    setFeedback(feedback.map((item)=> item.id === id ? { ...item, ...updItem} : item)
+    )
+  }
+
+  return (
+    <FeedbackContext.Provider
+      value={{ feedback, deleteFeedback, addFeedback, editFeedback, feedbackEdit, updateFeedback}}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
---
+++ src/data/feedbackdata.js
@@ -0,0 +1,19 @@
+const FeedbackData =[
+    {
+        id: 1,
+        rating: 10,
+        text: 'my rating is 10'
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'my rating is 9'
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'my rating is 8'
+    }
+]
+
+export default FeedbackData
